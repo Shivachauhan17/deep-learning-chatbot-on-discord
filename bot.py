8e81592b02910aa28c25024a67ddb1f1d33492e8
Shiva chauhan <103742975+Shivachauhan17@users.noreply.github.com>
2023-03-28 13:27:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,5 +17,5 @@ async def on_message(message):
         
         await message.channel.send(prediction_logic.predict(str(message)))
 
-client.run('MTA4ODA5MjgwMDQ0NjM4NjE4Nw.GIZSWm.5tKzFawaXvJmklUpM5-NIe_CAbwKW0hBZr0NVo')
+client.run('token here')
 
